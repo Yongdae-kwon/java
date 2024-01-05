ecb774fb092917ce65d9d08c618b8064601e1938
name <kydea2240@naver.com>
2024-01-05 10:29:27 +0900

    ch06 exam
    20번

---
+++ src/ch06/exam/BankAppliction.java
@@ -0,0 +1,134 @@
+package ch06.exam;
+import java.util.*;
+public class BankAppliction {
+
+	private static Account [] accountArray = new Account[100]; // 계좌 여러명 생성하기 위해 배열
+	private static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		boolean run = true;
+		
+		
+		while (run) {
+			System.out.println("-------------------------------------------------");
+			System.out.println("1.계좌생성 | 2.계좌목록 | 3.예금 | 4.출금 | 5.종료");
+			System.out.println("-------------------------------------------------");
+			System.out.print("선택>");
+			
+			int selectNo = Integer.parseInt(scan.nextLine());
+		
+		     if (selectNo == 1) {
+		    	 createAccount();
+			 }
+		     else if (selectNo == 2) {
+		    	 accountList();
+			 }
+		     else if (selectNo == 3) {
+		    	 deposit();
+		     }
+		     else if (selectNo == 4) {
+		    	 withdraw();
+		     }
+		     else if (selectNo == 5) {
+		    	  run = false;
+		     }
+		      
+	     }
+		System.out.print("프로그램 종료");
+	}
+	
+	private static void createAccount(){
+		System.out.println("-----------");
+		System.out.println("계좌생성");
+		System.out.println("-----------");
+		
+		System.out.print("계좌번호 :");
+		String ano = scan.nextLine();
+		System.out.print("계좌주 :");
+		String owner = scan.nextLine();
+		System.out.print("초기입금액 :");
+		int balance = Integer.parseInt(scan.nextLine());
+		
+		Account newAccount = new Account(ano, owner, balance); // 입력 받은 값을 객체 형태 생성
+		for (int i = 0; i<accountArray.length; i++) {          // accountArray 배열 이용
+			  if (accountArray[i] == null) {                   // 배열에 null (빈곳이 없는지 확인)
+				  accountArray[i] = newAccount;                // 배열 빈곳에 신규 가입자 데이터 넣기(객체형태로
+				  System.out.println("결과: 계좌가 생성되었습니다.");
+				  break;
+			  }
+		}		
+	}
+	private static void accountList() {
+		System.out.println("-----------");
+		System.out.println("계좌목록");
+		System.out.println("-----------");
+		
+		
+		for (int i = 0; i<accountArray.length; i++) {
+			Account account = accountArray [i]; // 배열에 들어가 있는 값들을 단일 객체 넣어서 처리
+			
+			if(account != null) {
+				System.out.print(account.getAno());
+				System.out.print("   ");
+				System.out.print(account.getOwner());
+				System.out.print("   ");
+				System.out.print(account.getBalance());
+				System.out.println("   ");
+			}			
+		}	
+	}
+	private static void deposit()    {
+		System.out.println("-----------");
+		System.out.println("예금");
+		System.out.println("-----------");
+		System.out.print("계좌번호 : ");
+		String ano = scan.nextLine();              // 계좌 번호 입력 : 검색
+		System.out.print("예금액 : ");
+		int money = Integer.parseInt(scan.nextLine()); // balance : 예금액
+		Account account = findAccount(ano);            // 계좌 번호가 일치하는 배열의 객체를 가져와
+		if (account == null) {
+			System.out.println("결과 : 계좌가업습니다.");
+			return;
+		}
+		account.setBalance(account.getBalance()+money);  // 배열에서 가져온 예금액 + 입력한 예금액을 증가 시킨 뒤 예금액 변환
+		System.out.println("계좌 : 예금이 성공이 되었습니다.");
+		
+	
+	}
+	private static void withdraw()   {
+		System.out.println("-----------");
+		System.out.println("출금");
+		System.out.println("-----------");
+		System.out.print("계좌번호 : ");
+		String ano = scan.nextLine();
+		System.out.print("출금액 : ");
+		int money = Integer.parseInt(scan.nextLine());	
+		
+		Account account = findAccount(ano);           // 계좌 번호가 일치한 배열을 받아오기.
+		if (account == null) {
+			System.out.println("결과 : 계좌가업습니다.");
+			return;			
+		}
+		account.setBalance(account.getBalance()-money);
+		System.out.println("계좌 : 출금이 성공이 되었습니다.");
+		
+	}
+	
+	private static Account findAccount(String ano) {
+		Account account = null;                                   // 담을 객체를 미리 선언
+		for (int i =0 ; i < accountArray.length ; i++) {          // 배열 안에 있는지 하나씩 확인
+			
+			if(accountArray[i] != null) {                         // 배열에 null이 아니면
+				String dbAno = accountArray[i].getAno();          // 배열 안에 있는 계좌번호 ano 가져와서
+				if (dbAno.equals(ano)) {                          // 입력했던 계좌번호 ano 와 배열에 있는 계좌번호 ano 비교
+					account = accountArray [i];                   // 일치하면 배열에 있는거를 객체 담기
+					break;
+				}
+			}
+		}
+		return account; //객체 반환		
+	}
+}
+
+
